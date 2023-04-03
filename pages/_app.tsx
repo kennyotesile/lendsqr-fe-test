6fa7e04503f02acf5b98ac3c58efbf2c2abf0ed7
Kehinde Otesile <kennyotesile@gmail.com>
2023-04-03 05:29:01 +0100

    Fix <title> and viewport meta tags warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Lendsqr | Empowering the smartest lenders</title>
+        <meta name="description" content="Launch your lending business in minutes and block borrowers with Lendsqr. Sign up now." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
