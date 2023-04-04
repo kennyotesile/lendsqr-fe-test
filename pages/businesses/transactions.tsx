6433645ca1d53a447a1bb5d3c4a2ca81fda676eb
Kehinde Otesile <kennyotesile@gmail.com>
2023-04-04 13:19:28 +0100

    Add pages (see body)
    This adds the following pages:
    
    - customers/users
    - customers/guarantors
    - customers/loans
    - customers/decision-models
    - customers/savings
    - customers/loan-requests
    - customers/whitelist
    - customers/karma
    
    - businesses/organization
    - businesses/loan-products
    - businesses/savings-products
    - businesses/fees-and-charges
    - businesses/transactions
    - businesses/services
    - businesses/service-account
    - businesses/settlements
    - businesses/reports
    
    - settings/preferences
    - settings/fees-and-pricing
    - settings/audit-logs
    - settings/systems-messages

---
+++ pages/businesses/transactions.tsx
@@ -0,0 +1,28 @@
+import TopNav from '@/components/TopNav'
+import Head from 'next/head'
+import SideNav from '@/components/SideNav'
+import EmptyPageMessage from '@/components/EmptyPageMessage'
+
+export default function Transactions() {
+  return (
+    <>
+      <Head>
+        <title>Transactions | Lendsqr</title>
+      </Head>
+      <div className='font-work-sans bg-whitespace-color h-screen pt-[100px]'>
+        <div className='absolute w-screen top-0'>
+          <TopNav />
+        </div>
+
+        <div className='h-full overflow-y-auto flex'>
+          <aside className='hidden h-full lg:block'>
+            <SideNav />
+          </aside>
+          <main className='w-full h-full flex items-center justify-center p-[24px] md:p-[30px] lg:p-[60px]'>
+            <EmptyPageMessage />
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
