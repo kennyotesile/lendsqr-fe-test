786b057cd88f875b3a6a67f28118118b371c2f2c
Kehinde Otesile <kennyotesile@gmail.com>
2023-04-04 13:38:00 +0100

    Fix grammatical error

--- components/EmptyPageMessage.tsx
+++ components/EmptyPageMessage.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function EmptyPageMessage() {
     return (
         <div className='text-center'>
-            Nothing to show here. Go to&nbsp;<Link href='/customers/users' className='text-primary-color hover:underline'>Users</Link>,&nbsp;
+            Nothing to see here for now. Go to&nbsp;<Link href='/customers/users' className='text-primary-color hover:underline'>Users</Link>,&nbsp;
             <Link href='/customers/users/user-details' className='text-primary-color hover:underline'>User Details</Link>&nbsp;or&nbsp;
             <Link href='/login' className='text-primary-color hover:underline'>Log in</Link>.
         </div>
