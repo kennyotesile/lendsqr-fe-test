229fb86a72ad4bb543d79785a917c92fd4c6e760
Kehinde Otesile <kennyotesile@gmail.com>
2023-04-05 15:27:52 +0100

    Fix breaking viewport on mobile screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Lendsqr | Empowering the smartest lenders</title>
         <meta name="description" content="Launch your lending business in minutes and block borrowers with Lendsqr. Sign up now." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         <link rel="icon" href="/favicon.png" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
