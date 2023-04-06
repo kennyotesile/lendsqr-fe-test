0970e9eaf052325c378e2bac09b02081f6c2860f
Kehinde Otesile <kennyotesile@gmail.com>
2023-04-06 09:58:41 +0100

    Fix broken link

--- components/EmptyPageMessage.tsx
+++ components/EmptyPageMessage.tsx
@@ -4,7 +4,7 @@ export default function EmptyPageMessage() {
     return (
         <div className='text-center'>
             Nothing to see here for now. Go to&nbsp;<Link href='/customers/users' className='text-primary-color hover:underline'>Users</Link>,&nbsp;
-            <Link href='/customers/users/user-details' className='text-primary-color hover:underline'>User Details</Link>&nbsp;or&nbsp;
+            <Link href='/customers/users/1' className='text-primary-color hover:underline'>User Details</Link>&nbsp;or&nbsp;
             <Link href='/login' className='text-primary-color hover:underline'>Log in</Link>.
         </div>
     );
